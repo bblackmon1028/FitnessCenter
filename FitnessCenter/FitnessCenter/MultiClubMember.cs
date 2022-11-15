375f1d36e05e0de11a9c6e5d126719f7f79a8268
June Dougherty <zachharlandougherty@gmail.com>
2022-11-14 21:27:50 -0500

    Members

---
+++ FitnessCenter/FitnessCenter/MultiClubMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenter
+{
+    public class MultiClubMember : Member
+    {
+        public int MemberPoints { get; private set; }
+
+        public override void CheckIn(Club club)
+        {
+            MemberPoints++;
+        }
+    }
+}
---
+++ FitnessCenter/FitnessCenter/SingleClubMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenter
+{
+    public class SingleMemberClub : Member
+    {
+        public string ClubMember { get; set; }
+        public override void CheckIn(Club club)
+        {
+
+            if (club != ClubMember)
+            {
+                throw new Exception("member does not belong to input club");
+            }
+
+        }
+    }
+}
