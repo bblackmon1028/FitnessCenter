fd9a49ca7507297cbf55201fa08b1b5ab3824181
craig <craig@ten987.com>
2022-11-17 22:25:11 -0500

    Read members from file instead of creating them manually

--- FitnessCenter/FitnessCenter/ManageMember.cs
+++ FitnessCenter/FitnessCenter/ManageMember.cs
@@ -8,8 +8,8 @@ namespace FitnessCenter
 
         public ManageMember()
         {
-            Members = new List<Member>() { new SingleClubMember(1, "Bob", "ABC Club"), new MultiClubMember(2, "Sue") };
-            //Members = FileManagement.ReadFile();
+            //Members = new List<Member>() { new SingleClubMember(1, "Bob", "ABC Club"), new MultiClubMember(2, "Sue") };
+            Members = FileManagement.ReadFile();
         }
 
         public void AddMember(string memberName)
