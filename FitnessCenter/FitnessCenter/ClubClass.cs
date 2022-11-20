b9693531b24cef1a78d389fb77ef310f02367ad0
June Dougherty <zachharlandougherty@gmail.com>
2022-11-20 17:24:54 -0500

    final changes for today

--- FitnessCenter/FitnessCenter/ClubClass.cs
+++ FitnessCenter/FitnessCenter/ClubClass.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
 namespace FitnessCenter
 {
     public class Club
